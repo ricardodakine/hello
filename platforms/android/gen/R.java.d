5f86a309feb7be49be24065ba3abcfc01bc9d198
ricardodakine <elevenpickles@gmail.com>
2014-01-01 23:55:27 -0800

    commit
    one, lol

---
+++ platforms/android/gen/R.java.d
@@ -0,0 +1,9 @@
+C:\hello\platforms\android\gen\com\example\hello\R.java \
+ : C:\hello\platforms\android\res\drawable\icon.png \
+C:\hello\platforms\android\res\drawable-hdpi\icon.png \
+C:\hello\platforms\android\res\drawable-ldpi\icon.png \
+C:\hello\platforms\android\res\drawable-mdpi\icon.png \
+C:\hello\platforms\android\res\drawable-xhdpi\icon.png \
+C:\hello\platforms\android\res\values\strings.xml \
+C:\hello\platforms\android\res\xml\config.xml \
+C:\hello\platforms\android\bin\AndroidManifest.xml \
